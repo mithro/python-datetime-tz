0c28933e2f7bd8c79af5facf7ab7ea8c1133417d
David Vincelli <david@freshbooks.com>
2011-11-28 16:51:12 -0500

    made setup.py degrade gracefully to distutils if setuptools is not found

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='python-datetime_tz',
