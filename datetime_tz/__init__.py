097df6154ee01dd5519828916e5e10d58c134b73
James Polley <jamezpolley@gmail.com>
2012-02-04 16:58:33 +1100

    Based on ocim's work at https://github.com/mithro/python-datetime-tz/pull/1
     - Don't use an explicit import to get the list of packages to an
       import; use the native __all__ to list the required imports
     - Import the appropriate version of dateutil based on python version
    
    NB: Not actually tested on python3

--- datetime_tz/__init__.py
+++ datetime_tz/__init__.py
@@ -1,12 +1,4 @@
-from .datetime_tz import (
-        datetime_tz,
-        detect_timezone,
-        iterate,
-        localtz,
-        localtz_set,
-        timedelta,
-        _detect_timezone_environ,
-        _detect_timezone_etc_localtime,
-        _detect_timezone_etc_timezone,
-        _detect_timezone_php
-    )
+__all__ = ['datetime_tz', 'detect_timezone', 'iterate', 'localtz',
+    'localtz_set', 'timedelta', '_detect_timezone_environ',
+    '_detect_timezone_etc_localtime', '_detect_timezone_etc_timezone',
+    '_detect_timezone_php']
--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(
+import sys
+
+data = dict(
     name='python-datetime_tz',
     version='0.1',
     author='Tim Ansell',
     author_email='mithro@mithis.com',
     packages=['datetime_tz'],
-    install_requires=[
-        'python-dateutil == 1.5',
-        'pytz'
-    ]
+    install_requires=['pytz'],
 )
+
+if sys.version[:3] < '3.0':
+    data['install_requires'].append('python.dateutil == 1.5')
+else:
+    data['install_requires'].append('python.dateutil == 2.0')
+
+setup(**data)
