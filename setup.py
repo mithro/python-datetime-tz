81ebfa28a9eb168a032d46c590ab0ba5301dc19d
David Vincelli <david@freshbooks.com>
2012-03-15 11:44:55 -0400

    Use single file python module packaging, added test_suite settings too

--- setup.py
+++ setup.py
@@ -3,14 +3,21 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(
+import sys
+
+data = dict(
     name='python-datetime_tz',
     version='0.1',
     author='Tim Ansell',
     author_email='mithro@mithis.com',
-    packages=['datetime_tz'],
-    install_requires=[
-        'python-dateutil == 1.5',
-        'pytz'
-    ]
+    install_requires=['pytz'],
+    py_modules=['datetime_tz','pytz_abbr'],
+    test_suite='tests'
 )
+
+if sys.version[:3] < '3.0':
+    data['install_requires'].append('python-dateutil == 1.5')
+else:
+    data['install_requires'].append('python-dateutil == 2.0')
+
+setup(**data)
