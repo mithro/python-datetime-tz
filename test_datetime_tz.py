3e924b2c4eb0d59372cc88f6bf15a7317ab4592c
davidm <davidm@25043fe6-1616-0410-a82d-ba8f324cfe61>
2012-11-08 13:22:22 +0000

    Added tests for the other comparisons
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@16492 25043fe6-1616-0410-a82d-ba8f324cfe61

--- test_datetime_tz.py
+++ test_datetime_tz.py
@@ -12,3 +12,28 @@ def test_compare_tz_second():
     a = datetime.datetime(2012,3,4,1,2,3)
     b = datetime_tz.datetime_tz(2012,3,4,1,2,3)
     assert b == a
+
+def test_compare_greater():
+    a = datetime.datetime(2012,3,4,3,2,3)
+    b = datetime_tz.datetime_tz(2012,3,4,1,2,3)
+    assert a > b
+    assert a >= b
+    a = datetime.datetime(2012,3,3,3,2,3)
+    assert b > a
+    assert b >= a
+
+def test_compare_less():
+    a = datetime.datetime(2012,3,4,3,2,3)
+    b = datetime_tz.datetime_tz(2012,3,4,1,2,3)
+    assert b < a
+    assert b <= a
+    a = datetime.datetime(2012,3,3,3,2,3)
+    assert a < b
+    assert a <= b
+
+def test_compare_notequal():
+    a = datetime.datetime(2012,3,4,3,2,3)
+    b = datetime_tz.datetime_tz(2012,3,4,1,2,3)
+    assert a != b
+    assert b != a
+
