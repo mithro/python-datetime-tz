a22776ae18cc4204d72ed0b683092a802779c985
Tim 'mithro' Ansell <mithro@mithis.com>
2015-06-12 13:39:45 +1000

    Initial script to run datetime-tz against all pytz versions.

---
+++ run_tests_all_pytz.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set ts=2 sw=2 et sts=2 ai:
+#
+# Copyright 2015 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import print_function
+
+"""Run the tests against every pytz version available."""
+
+__author__ = "tansell@google.com (Tim Ansell)"
+
+try:
+  import simplejson
+except ImportError:
+  import json as simplejson
+
+import pprint
+import subprocess
+import sys
+import urllib
+
+if not hasattr(sys, 'real_prefix'):
+  print("""\
+This script should only be run inside a virtualenv because it is going to
+modify the install version of things.
+""")
+  sys.exit(1)
+
+# Get the pytz versions from pypi
+pypi_data_raw = urllib.urlopen('https://pypi.python.org/pypi/pytz/json').read()
+pypi_data = simplejson.loads(pypi_data_raw)
+
+releases = pypi_data['releases']
+success = []
+failures = []
+for release in sorted(releases):
+  # Skip very old versions
+  if release < "2007g":
+    print("Skipping release", release, "as it is isn't supported")
+    continue
+
+  print()
+  print("Running tests with pytz release", release)
+  print("="*75)
+  print("Installing...")
+  subprocess.check_call('pip install --use-mirrors --force-reinstall pytz=='+release, shell=True)
+  print("-"*75)
+  print("Running tests...")
+  t = subprocess.Popen('python setup.py test', shell=True)
+  if t.wait() != 0:
+    failures.append(release)
+  else:
+    success.append(release)
+    #import time
+    #time.sleep(30)
+  print("="*75)
+
+print("Tests passed on pytz versions:")
+pprint.pprint(success)
+print()
+print("Tests failed on pytz versions:")
+pprint.pprint(failures)
+print("="*75)
+
+if len(failures) > 0:
+  sys.exit(1)
