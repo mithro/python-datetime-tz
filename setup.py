a66661c67520f82d18614bc4d8a150c015aeedec
Tim 'mithro' Ansell <mithro@mithis.com>
2015-06-30 15:48:25 +1000

    Version bump for release.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ import sys
 
 data = dict(
     name='python-datetime-tz',
-    version='0.2',
+    version='0.3',
     author='Tim Ansell',
     author_email='mithro@mithis.com',
     url='http://github.com/mithro/python-datetime-tz',
