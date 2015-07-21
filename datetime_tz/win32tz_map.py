52254e9faf5d5d8d953c1822cdad8167b8b10112
David Moore <davidm@j5int.com>
2015-07-21 15:58:13 +0200

    Latest map

--- datetime_tz/win32tz_map.py
+++ datetime_tz/win32tz_map.py
@@ -1,101 +1,110 @@
-'''Map between Windows an Olson timezones taken from http://www.unicode.org/repos/cldr/trunk/common/supplemental/windowsZones.xml
+'''Map between Windows and Olson timezones taken from http://www.unicode.org/repos/cldr/trunk/common/supplemental/windowsZones.xml
 Generated automatically from datetime_tz.py'''
-source_hash = '219ad1cf41309618a241ccf36a6b7ef1' # md5 sum of xml source data
+source_hash = '2d9188183b4c4f67825b6c9d15505e80' # md5 sum of xml source data
 win32timezones = {
-    u'AUS Central Standard Time': u'Australia/Darwin', # S (GMT+09:30) Darwin
-    u'AUS Eastern Standard Time': u'Australia/Sydney', # D (GMT+10:00) Canberra, Melbourne, Sydney
-    u'Afghanistan Standard Time': u'Asia/Kabul', # S (GMT+04:30) Kabul
-    u'Alaskan Standard Time': u'America/Anchorage', # D (GMT-09:00) Alaska
-    u'Arab Standard Time': u'Asia/Riyadh', # S (GMT+03:00) Kuwait, Riyadh
-    u'Arabian Standard Time': u'Asia/Dubai', # S (GMT+04:00) Abu Dhabi, Muscat
-    u'Arabic Standard Time': u'Asia/Baghdad', # S (GMT+03:00) Baghdad
-    u'Argentina Standard Time': u'America/Buenos_Aires', # D (GMT-03:00) Buenos Aires
-    u'Armenian Standard Time': u'Asia/Yerevan', # D [XP] (GMT+04:00) Yerevan
-    u'Atlantic Standard Time': u'America/Halifax', # D (GMT-04:00) Atlantic Time (Canada)
-    u'Azerbaijan Standard Time': u'Asia/Baku', # D (GMT+04:00) Baku
-    u'Azores Standard Time': u'Atlantic/Azores', # D (GMT-01:00) Azores
-    u'Bangladesh Standard Time': u'Asia/Dhaka', # D (GMT+06:00) Dhaka
-    u'Canada Central Standard Time': u'America/Regina', # S (GMT-06:00) Saskatchewan
-    u'Cape Verde Standard Time': u'Atlantic/Cape_Verde', # S (GMT-01:00) Cape Verde Is.
-    u'Caucasus Standard Time': u'Asia/Yerevan', # D (GMT+04:00) Yerevan / S [XP] (GMT+04:00) Caucasus Standard Time
-    u'Cen. Australia Standard Time': u'Australia/Adelaide', # D (GMT+09:30) Adelaide
-    u'Central America Standard Time': u'America/Guatemala', # S (GMT-06:00) Central America
-    u'Central Asia Standard Time': u'Asia/Almaty', # S (GMT+06:00) Astana
-    u'Central Brazilian Standard Time': u'America/Campo_Grande', # D (GMT-04:00) Manaus
-    u'Central Europe Standard Time': u'Europe/Budapest', # D (GMT+01:00) Belgrade, Bratislava, Budapest, Ljubljana, Prague
-    u'Central European Standard Time': u'Europe/Warsaw', # D (GMT+01:00) Sarajevo, Skopje, Warsaw, Zagreb
-    u'Central Pacific Standard Time': u'Pacific/Guadalcanal', # S (GMT+11:00) Magadan, Solomon Is., New Caledonia
-    u'Central Standard Time': u'America/Chicago', # D (GMT-06:00) Central Time (US & Canada)
-    u'Central Standard Time (Mexico)': u'America/Mexico_City', # D (GMT-06:00) Guadalajara, Mexico City, Monterrey
-    u'China Standard Time': u'Asia/Shanghai', # S (GMT+08:00) Beijing, Chongqing, Hong Kong, Urumqi
-    u'Dateline Standard Time': u'Etc/GMT+12', # S (GMT-12:00) International Date Line West
-    u'E. Africa Standard Time': u'Africa/Nairobi', # S (GMT+03:00) Nairobi
-    u'E. Australia Standard Time': u'Australia/Brisbane', # S (GMT+10:00) Brisbane
-    u'E. Europe Standard Time': u'Europe/Minsk', # D (GMT+02:00) Minsk
-    u'E. South America Standard Time': u'America/Sao_Paulo', # D (GMT-03:00) Brasilia
-    u'Eastern Standard Time': u'America/New_York', # D (GMT-05:00) Eastern Time (US & Canada)
-    u'Egypt Standard Time': u'Africa/Cairo', # D (GMT+02:00) Cairo
-    u'Ekaterinburg Standard Time': u'Asia/Yekaterinburg', # D (GMT+05:00) Ekaterinburg
-    u'FLE Standard Time': u'Europe/Kiev', # D (GMT+02:00) Helsinki, Kyiv, Riga, Sofia, Tallinn, Vilnius
-    u'Fiji Standard Time': u'Pacific/Fiji', # D (GMT+12:00) Fiji, Marshall Is.
-    u'GMT Standard Time': u'Europe/London', # D (GMT) Greenwich Mean Time : Dublin, Edinburgh, Lisbon, London
-    u'GTB Standard Time': u'Europe/Istanbul', # D (GMT+02:00) Athens, Bucharest, Istanbul
-    u'Georgian Standard Time': u'Etc/GMT-3', # S (GMT+03:00) Tbilisi
-    u'Greenland Standard Time': u'America/Godthab', # D (GMT-03:00) Greenland
-    u'Greenwich Standard Time': u'Atlantic/Reykjavik', # S (GMT) Monrovia, Reykjavik
-    u'Hawaiian Standard Time': u'Pacific/Honolulu', # S (GMT-10:00) Hawaii
-    u'India Standard Time': u'Asia/Calcutta', # S (GMT+05:30) Chennai, Kolkata, Mumbai, New Delhi
-    u'Iran Standard Time': u'Asia/Tehran', # D (GMT+03:30) Tehran
-    u'Israel Standard Time': u'Asia/Jerusalem', # D (GMT+02:00) Jerusalem
-    u'Jordan Standard Time': u'Asia/Amman', # D (GMT+02:00) Amman
-    u'Kamchatka Standard Time': u'Asia/Kamchatka', # D (GMT+12:00) Petropavlovsk-Kamchatsky
-    u'Korea Standard Time': u'Asia/Seoul', # S (GMT+09:00) Seoul
-    u'Mauritius Standard Time': u'Indian/Mauritius', # D (GMT+04:00) Port Louis
-    u'Mexico Standard Time': u'America/Mexico_City', # D [XP] (GMT-06:00) Guadalajara, Mexico City, Monterrey - Old
-    u'Mexico Standard Time 2': u'America/Chihuahua', # D [XP] (GMT-07:00) Chihuahua, La Paz, Mazatlan - Old
-    u'Mid-Atlantic Standard Time': u'Etc/GMT+2', # D (GMT-02:00) Mid-Atlantic
-    u'Middle East Standard Time': u'Asia/Beirut', # D (GMT+02:00) Beirut
-    u'Montevideo Standard Time': u'America/Montevideo', # D (GMT-03:00) Montevideo
-    u'Morocco Standard Time': u'Africa/Casablanca', # D (GMT) Casablanca
-    u'Mountain Standard Time': u'America/Denver', # D (GMT-07:00) Mountain Time (US & Canada)
-    u'Mountain Standard Time (Mexico)': u'America/Chihuahua', # (GMT-07:00) Chihuahua, La Paz, Mazatlan
-    u'Myanmar Standard Time': u'Asia/Rangoon', # S (GMT+06:30) Yangon (Rangoon)
-    u'N. Central Asia Standard Time': u'Asia/Novosibirsk', # D (GMT+06:00) Novosibirsk
-    u'Namibia Standard Time': u'Africa/Windhoek', # D (GMT+02:00) Windhoek
-    u'Nepal Standard Time': u'Asia/Katmandu', # S (GMT+05:45) Kathmandu
-    u'New Zealand Standard Time': u'Pacific/Auckland', # D (GMT+12:00) Auckland, Wellington
-    u'Newfoundland Standard Time': u'America/St_Johns', # D (GMT-03:30) Newfoundland
-    u'North Asia East Standard Time': u'Asia/Irkutsk', # D (GMT+08:00) Irkutsk
-    u'North Asia Standard Time': u'Asia/Krasnoyarsk', # D (GMT+07:00) Krasnoyarsk
-    u'Pacific SA Standard Time': u'America/Santiago', # D (GMT-04:00) Santiago
-    u'Pacific Standard Time': u'America/Los_Angeles', # D (GMT-08:00) Pacific Time (US & Canada)
-    u'Pacific Standard Time (Mexico)': u'America/Tijuana', # D (GMT-08:00) Tijuana, Baja California
-    u'Pakistan Standard Time': u'Asia/Karachi', # D (GMT+05:00) Islamabad, Karachi
-    u'Paraguay Standard Time': u'America/Asuncion', # (GMT-04:00) Asuncion
-    u'Romance Standard Time': u'Europe/Paris', # D (GMT+01:00) Brussels, Copenhagen, Madrid, Paris
-    u'Russian Standard Time': u'Europe/Moscow', # D (GMT+03:00) Moscow, St. Petersburg, Volgograd
-    u'SA Eastern Standard Time': u'America/Cayenne', # S (GMT-03:00) Cayenne
-    u'SA Pacific Standard Time': u'America/Bogota', # S (GMT-05:00) Bogota, Lima, Quito
-    u'SA Western Standard Time': u'America/La_Paz', # S (GMT-04:00) Georgetown, La Paz, San Juan
-    u'SE Asia Standard Time': u'Asia/Bangkok', # S (GMT+07:00) Bangkok, Hanoi, Jakarta
-    u'Samoa Standard Time': u'Pacific/Apia', # S (GMT-11:00) Midway Island, Samoa
-    u'Singapore Standard Time': u'Asia/Singapore', # S (GMT+08:00) Kuala Lumpur, Singapore
-    u'South Africa Standard Time': u'Africa/Johannesburg', # S (GMT+02:00) Harare, Pretoria
-    u'Sri Lanka Standard Time': u'Asia/Colombo', # S (GMT+05:30) Sri Jayawardenepura
-    u'Taipei Standard Time': u'Asia/Taipei', # S (GMT+08:00) Taipei
-    u'Tasmania Standard Time': u'Australia/Hobart', # D (GMT+10:00) Hobart
-    u'Tokyo Standard Time': u'Asia/Tokyo', # S (GMT+09:00) Osaka, Sapporo, Tokyo
-    u'Tonga Standard Time': u'Pacific/Tongatapu', # S (GMT+13:00) Nuku'alofa
-    u'US Eastern Standard Time': u'Etc/GMT+5', # S (GMT-05:00) Indiana (East)
-    u'US Mountain Standard Time': u'America/Phoenix', # S (GMT-07:00) Arizona
-    u'UTC': u'Etc/GMT', # S (GMT) Coordinated Universal Time
-    u'Ulaanbaatar Standard Time': u'Asia/Ulaanbaatar', # (GMT+08:00) Ulaanbaatar
-    u'Venezuela Standard Time': u'America/Caracas', # S (GMT-04:30) Caracas
-    u'Vladivostok Standard Time': u'Asia/Vladivostok', # D (GMT+10:00) Vladivostok
-    u'W. Australia Standard Time': u'Australia/Perth', # D (GMT+08:00) Perth
-    u'W. Central Africa Standard Time': u'Africa/Lagos', # S (GMT+01:00) West Central Africa
-    u'W. Europe Standard Time': u'Europe/Berlin', # D (GMT+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna
-    u'West Asia Standard Time': u'Asia/Tashkent', # S (GMT+05:00) Tashkent
-    u'West Pacific Standard Time': u'Pacific/Port_Moresby', # S (GMT+10:00) Guam, Port Moresby
-    u'Yakutsk Standard Time': u'Asia/Yakutsk', # D (GMT+09:00) Yakutsk
+    u'AUS Central Standard Time': u'Australia/Darwin', # (UTC+09:30) Darwin
+    u'AUS Eastern Standard Time': u'Australia/Sydney', # (UTC+10:00) Canberra, Melbourne, Sydney
+    u'Afghanistan Standard Time': u'Asia/Kabul', # (UTC+04:30) Kabul
+    u'Alaskan Standard Time': u'America/Anchorage', # (UTC-09:00) Alaska
+    u'Arab Standard Time': u'Asia/Aden', # (UTC+03:00) Kuwait, Riyadh
+    u'Arabian Standard Time': u'Etc/GMT-4', # (UTC+04:00) Abu Dhabi, Muscat
+    u'Arabic Standard Time': u'Asia/Baghdad', # (UTC+03:00) Baghdad
+    u'Argentina Standard Time': u'America/Buenos_Aires', # (UTC-03:00) Buenos Aires
+    u'Atlantic Standard Time': u'America/Thule', # (UTC-04:00) Atlantic Time (Canada)
+    u'Azerbaijan Standard Time': u'Asia/Baku', # (UTC+04:00) Baku
+    u'Azores Standard Time': u'Atlantic/Azores', # (UTC-01:00) Azores
+    u'Bahia Standard Time': u'America/Bahia', # (UTC-03:00) Salvador
+    u'Bangladesh Standard Time': u'Asia/Thimphu', # (UTC+06:00) Dhaka
+    u'Belarus Standard Time': u'Europe/Minsk', # (UTC+03:00) Minsk
+    u'Canada Central Standard Time': u'America/Regina', # (UTC-06:00) Saskatchewan
+    u'Cape Verde Standard Time': u'Etc/GMT+1', # (UTC-01:00) Cabo Verde Is.
+    u'Caucasus Standard Time': u'Asia/Yerevan', # (UTC+04:00) Yerevan
+    u'Cen. Australia Standard Time': u'Australia/Adelaide', # (UTC+09:30) Adelaide
+    u'Central America Standard Time': u'Etc/GMT+6', # (UTC-06:00) Central America
+    u'Central Asia Standard Time': u'Etc/GMT-6', # (UTC+06:00) Astana
+    u'Central Brazilian Standard Time': u'America/Cuiaba', # (UTC-04:00) Cuiaba
+    u'Central Europe Standard Time': u'Europe/Bratislava', # (UTC+01:00) Belgrade, Bratislava, Budapest, Ljubljana, Prague
+    u'Central European Standard Time': u'Europe/Warsaw', # (UTC+01:00) Sarajevo, Skopje, Warsaw, Zagreb
+    u'Central Pacific Standard Time': u'Etc/GMT-11', # (UTC+11:00) Solomon Is., New Caledonia
+    u'Central Standard Time': u'CST6CDT', # (UTC-06:00) Central Time (US & Canada)
+    u'Central Standard Time (Mexico)': u'America/Mexico_City', # (UTC-06:00) Guadalajara, Mexico City, Monterrey
+    u'China Standard Time': u'Asia/Macau', # (UTC+08:00) Beijing, Chongqing, Hong Kong, Urumqi
+    u'Dateline Standard Time': u'Etc/GMT+12', # (UTC-12:00) International Date Line West
+    u'E. Africa Standard Time': u'Etc/GMT-3', # (UTC+03:00) Nairobi
+    u'E. Australia Standard Time': u'Australia/Brisbane', # (UTC+10:00) Brisbane
+    u'E. South America Standard Time': u'America/Sao_Paulo', # (UTC-03:00) Brasilia
+    u'Eastern Standard Time': u'EST5EDT', # (UTC-05:00) Eastern Time (US & Canada)
+    u'Eastern Standard Time (Mexico)': u'America/Cancun', # (UTC-05:00) Chetumal
+    u'Egypt Standard Time': u'Africa/Cairo', # (UTC+02:00) Cairo
+    u'Ekaterinburg Standard Time': u'Asia/Yekaterinburg', # (UTC+05:00) Ekaterinburg (RTZ 4)
+    u'FLE Standard Time': u'Europe/Kiev', # (UTC+02:00) Helsinki, Kyiv, Riga, Sofia, Tallinn, Vilnius
+    u'Fiji Standard Time': u'Pacific/Fiji', # (UTC+12:00) Fiji
+    u'GMT Standard Time': u'Europe/Lisbon', # (UTC) Dublin, Edinburgh, Lisbon, London
+    u'GTB Standard Time': u'Europe/Bucharest', # (UTC+02:00) Athens, Bucharest
+    u'Georgian Standard Time': u'Asia/Tbilisi', # (UTC+04:00) Tbilisi
+    u'Greenland Standard Time': u'America/Godthab', # (UTC-03:00) Greenland
+    u'Greenwich Standard Time': u'Africa/Lome', # (UTC) Monrovia, Reykjavik
+    u'Hawaiian Standard Time': u'Etc/GMT+10', # (UTC-10:00) Hawaii
+    u'India Standard Time': u'Asia/Calcutta', # (UTC+05:30) Chennai, Kolkata, Mumbai, New Delhi
+    u'Iran Standard Time': u'Asia/Tehran', # (UTC+03:30) Tehran
+    u'Israel Standard Time': u'Asia/Jerusalem', # (UTC+02:00) Jerusalem
+    u'Jordan Standard Time': u'Asia/Amman', # (UTC+02:00) Amman
+    u'Kaliningrad Standard Time': u'Europe/Kaliningrad', # (UTC+02:00) Kaliningrad (RTZ 1)
+    u'Korea Standard Time': u'Asia/Seoul', # (UTC+09:00) Seoul
+    u'Libya Standard Time': u'Africa/Tripoli', # (UTC+02:00) Tripoli
+    u'Line Islands Standard Time': u'Etc/GMT-14', # (UTC+14:00) Kiritimati Island
+    u'Magadan Standard Time': u'Asia/Magadan', # (UTC+10:00) Magadan
+    u'Mauritius Standard Time': u'Indian/Mahe', # (UTC+04:00) Port Louis
+    u'Middle East Standard Time': u'Asia/Beirut', # (UTC+02:00) Beirut
+    u'Montevideo Standard Time': u'America/Montevideo', # (UTC-03:00) Montevideo
+    u'Morocco Standard Time': u'Africa/Casablanca', # (UTC) Casablanca
+    u'Mountain Standard Time': u'MST7MDT', # (UTC-07:00) Mountain Time (US & Canada)
+    u'Mountain Standard Time (Mexico)': u'America/Chihuahua', # (UTC-07:00) Chihuahua, La Paz, Mazatlan
+    u'Myanmar Standard Time': u'Asia/Rangoon', # (UTC+06:30) Yangon (Rangoon)
+    u'N. Central Asia Standard Time': u'Asia/Novosibirsk', # (UTC+06:00) Novosibirsk (RTZ 5)
+    u'Namibia Standard Time': u'Africa/Windhoek', # (UTC+01:00) Windhoek
+    u'Nepal Standard Time': u'Asia/Katmandu', # (UTC+05:45) Kathmandu
+    u'New Zealand Standard Time': u'Pacific/Auckland', # (UTC+12:00) Auckland, Wellington
+    u'Newfoundland Standard Time': u'America/St_Johns', # (UTC-03:30) Newfoundland
+    u'North Asia East Standard Time': u'Asia/Irkutsk', # (UTC+08:00) Irkutsk (RTZ 7)
+    u'North Asia Standard Time': u'Asia/Krasnoyarsk', # (UTC+07:00) Krasnoyarsk (RTZ 6)
+    u'Pacific SA Standard Time': u'America/Santiago', # (UTC-03:00) Santiago
+    u'Pacific Standard Time': u'PST8PDT', # (UTC-08:00) Pacific Time (US & Canada)
+    u'Pacific Standard Time (Mexico)': u'America/Santa_Isabel', # (UTC-08:00) Baja California
+    u'Pakistan Standard Time': u'Asia/Karachi', # (UTC+05:00) Islamabad, Karachi
+    u'Paraguay Standard Time': u'America/Asuncion', # (UTC-04:00) Asuncion
+    u'Romance Standard Time': u'Europe/Paris', # (UTC+01:00) Brussels, Copenhagen, Madrid, Paris
+    u'Russia Time Zone 10': u'Asia/Srednekolymsk', # (UTC+11:00) Chokurdakh (RTZ 10)
+    u'Russia Time Zone 11': u'Asia/Kamchatka', # (UTC+12:00) Anadyr, Petropavlovsk-Kamchatsky (RTZ 11)
+    u'Russia Time Zone 3': u'Europe/Samara', # (UTC+04:00) Izhevsk, Samara (RTZ 3)
+    u'Russian Standard Time': u'Europe/Moscow', # (UTC+03:00) Moscow, St. Petersburg, Volgograd (RTZ 2)
+    u'SA Eastern Standard Time': u'Etc/GMT+3', # (UTC-03:00) Cayenne, Fortaleza
+    u'SA Pacific Standard Time': u'Etc/GMT+5', # (UTC-05:00) Bogota, Lima, Quito, Rio Branco
+    u'SA Western Standard Time': u'Etc/GMT+4', # (UTC-04:00) Georgetown, La Paz, Manaus, San Juan
+    u'SE Asia Standard Time': u'Etc/GMT-7', # (UTC+07:00) Bangkok, Hanoi, Jakarta
+    u'Samoa Standard Time': u'Pacific/Apia', # (UTC+13:00) Samoa
+    u'Singapore Standard Time': u'Etc/GMT-8', # (UTC+08:00) Kuala Lumpur, Singapore
+    u'South Africa Standard Time': u'Etc/GMT-2', # (UTC+02:00) Harare, Pretoria
+    u'Sri Lanka Standard Time': u'Asia/Colombo', # (UTC+05:30) Sri Jayawardenepura
+    u'Syria Standard Time': u'Asia/Damascus', # (UTC+02:00) Damascus
+    u'Taipei Standard Time': u'Asia/Taipei', # (UTC+08:00) Taipei
+    u'Tasmania Standard Time': u'Australia/Hobart', # (UTC+10:00) Hobart
+    u'Tokyo Standard Time': u'Etc/GMT-9', # (UTC+09:00) Osaka, Sapporo, Tokyo
+    u'Tonga Standard Time': u'Etc/GMT-13', # (UTC+13:00) Nuku'alofa
+    u'Turkey Standard Time': u'Europe/Istanbul', # (UTC+02:00) Istanbul
+    u'US Eastern Standard Time': u'America/Indianapolis', # (UTC-05:00) Indiana (East)
+    u'US Mountain Standard Time': u'Etc/GMT+7', # (UTC-07:00) Arizona
+    u'UTC': u'Etc/GMT', # (UTC) Coordinated Universal Time
+    u'UTC+12': u'Etc/GMT-12', # (UTC+12:00) Coordinated Universal Time+12
+    u'UTC-02': u'Etc/GMT+2', # (UTC-02:00) Coordinated Universal Time-02
+    u'UTC-11': u'Etc/GMT+11', # (UTC-11:00) Coordinated Universal Time-11
+    u'Ulaanbaatar Standard Time': u'Asia/Ulaanbaatar', # (UTC+08:00) Ulaanbaatar
+    u'Venezuela Standard Time': u'America/Caracas', # (UTC-04:30) Caracas
+    u'Vladivostok Standard Time': u'Asia/Vladivostok', # (UTC+10:00) Vladivostok, Magadan (RTZ 9)
+    u'W. Australia Standard Time': u'Australia/Perth', # (UTC+08:00) Perth
+    u'W. Central Africa Standard Time': u'Etc/GMT-1', # (UTC+01:00) West Central Africa
+    u'W. Europe Standard Time': u'Europe/Vatican', # (UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna
+    u'West Asia Standard Time': u'Etc/GMT-5', # (UTC+05:00) Ashgabat, Tashkent
+    u'West Pacific Standard Time': u'Etc/GMT-10', # (UTC+10:00) Guam, Port Moresby
+    u'Yakutsk Standard Time': u'Asia/Yakutsk', # (UTC+09:00) Yakutsk (RTZ 8)
 }
